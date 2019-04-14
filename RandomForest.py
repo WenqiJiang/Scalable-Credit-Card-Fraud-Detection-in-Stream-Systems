d37c6995e2b1e4ad2bc8913559646350cf6e440d
Rosberg <captainlmy@gmail.com>
2019-04-13 20:49:04 -0400

    add boolean 'use_origin' choice

--- RandomForest.py
+++ RandomForest.py
@@ -1,11 +1,14 @@
 import numpy as np
-from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+
+# load features and label from training set into array
+
 
 use_origin = False
 
 # if use_origin = True, use original dataset
 if use_origin:
-    
+
     train_features = np.load("./data/origin_data/X_train.npy")
     train_label = np.load("./data/origin_data/y_train.npy")
 
@@ -23,9 +26,12 @@ else:
     val_features = np.load("./data/subsamp_data/processed_X_val.npy")
     val_label = np.load("./data/subsamp_data/processed_y_val.npy")
 
-svc = SVC(C=100,class_weight = {0:1,1:100}).fit(train_features,train_label)        
 
-val_label_predict = svc.predict(val_features)
+clf = RandomForestClassifier(n_estimators=100, max_depth=10,random_state=0,class_weight = {0:1,1:100})
+
+clf.fit(train_features,train_label)
+
+val_label_predict = clf.predict(val_features)
 
 TP = 0
 FP = 0
@@ -57,4 +63,4 @@ print('FN count:    {}'.format(FN))
 print('Precision rate:  {}'.format(TP/(TP+FP)))
 print('Recall rate: {}'.format(TP/(TP+FN)))
 
-print('Mean score:  {}'.format(svc.score(val_features,val_label)))
+print('Mean score:  {}'.format(clf.score(val_features,val_label)))
